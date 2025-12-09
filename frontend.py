65df7365b823b744a2d0839298906bef2ce3c007
Sujal Parmar <119657662+sujalparmarr@users.noreply.github.com>
2025-12-09 23:38:28 +0530

    Add files via upload

---
+++ frontend.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import requests
+
+API_URL = "http://localhost:8000/shorten"
+
+st.set_page_config(page_title="URL Shortener", page_icon="🔗")
+
+st.title("🔗 Simple URL Shortener (FastAPI + Streamlit)")
+
+st.write("Enter a long URL and get a short version instantly!")
+
+long_url = st.text_input("Enter URL to shorten:")
+
+if st.button("Shorten URL"):
+    if long_url:
+        with st.spinner("Generating short URL..."):
+            response = requests.post(API_URL, json={"long_url": long_url})
+            
+            if response.status_code == 200:
+                data = response.json()
+                
+                st.success("Short URL Generated!")
+                st.write("### 🔗 Short URL:")
+                st.markdown(f"[{data['short_ur 4l']}]({data['short_url']})")
+            else:
+                st.error("Failed to shorten URL. Check if the API is running.")
+    else:
+        st.warning("Please enter a valid URL.")
